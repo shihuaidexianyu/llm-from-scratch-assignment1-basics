ee4e3d1b5cd7067a82c771bb93549e5e5338ccc8
shihuaidexianyu <exqinhongwei@outlook.com>
2026-01-22 15:28:10 +0800

    feat: 添加 MyRMSNorm 类并在 run_rmsnorm 函数中使用它进行 RMSNorm 操作

---
+++ cs336_basics/rmsnorm.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+
+class MyRMSNorm(nn.Module):
+    def __init__(
+        self, dim: int, eps: float = 1e-5, device: torch.device | None = None, dtype: torch.dtype | None = None
+    ):
+        """
+        RMSNorm layer
+        """
+        super().__init__()
+        self.eps = eps
+        self.scale = nn.Parameter(torch.ones((dim,), device=device, dtype=dtype))  # 参数向量, 初始化为1
+
+    def forward(self, input: Tensor) -> Tensor:
+        rms = input.pow(2).mean(-1, keepdim=True).add(self.eps).sqrt()  # 计算RMS
+        return input / rms * self.scale  # 逐位相乘
--- tests/adapters.py
+++ tests/adapters.py
@@ -386,7 +386,11 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    from cs336_basics.rmsnorm import MyRMSNorm
+
+    rmsnorm = MyRMSNorm(d_model, eps)
+    rmsnorm.scale.data = weights
+    return rmsnorm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
